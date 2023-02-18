9913e6be718fbad3800a91e862f08dda2e415a86
Anton Horodchuk <antoman1994@gmail.com>
2023-02-18 20:05:44 +0200

    Applying black suggestions

--- src/odoo_tasks_management/odoo/module.py
+++ src/odoo_tasks_management/odoo/module.py
@@ -5,7 +5,6 @@ from odoo_tasks_management.settings import Settings
 
 
 class OdooClientModule(injector.Module):
-
     @injector.provider
     def _odoo_client(self, settings: Settings) -> OdooClient:
         return OdooClient(settings)
